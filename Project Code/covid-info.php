37b1eb5bca4bc063af5d05046e15058760fb589d
Ryan Bharath <dbossman60@gmail.com>
2021-11-13 20:28:02 -0500

    changed formatting

--- Project Code/covid-info.php
+++ Project Code/covid-info.php
@@ -15,7 +15,7 @@ include_once './includes/header.php'
     <p> The official name for COVID-19 is SARS-CoV-2 or serve acute respiratory syndrome coronavirus 2.</p>
     <p> COVID-19 is a disease that mainly attacks the respiratory system in one's body.</p>
     <p> Although some may contract the disease not all cases are the same with some being more impacted than others. </p>
-    <p>This disease when first started in America attacked more of the older generation however nowadays cases include any age range.</p>
+    <p> This disease when first started in America attacked more of the older generation however nowadays cases include any age range.</p>
     <p> With the increase of cases there was a vaccine created by companies such as Pfizer and Moderna trying to combat the disease.</p>
     <p> Although with efforts of trying to get rid of this disease, the disease has grown more in different variants such as the Alpha and Delta Variant.</p>
 </div>
